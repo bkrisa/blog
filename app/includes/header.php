31a83ad6ba056b612ed8c86918622d64a3fb29c0
bkrisa <web.bkrisa@gmail.com>
2026-09-07 16:41:21 +0200

    meta: twitter widgets

--- app/includes/header.php
+++ app/includes/header.php
@@ -43,6 +43,7 @@ if (!defined('SECURE_ACCESS')) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="twitter:widgets:csp" content="on">
 
   <title><?php echo htmlspecialchars($page_title); ?></title>
   <meta name="description" content="<?php echo htmlspecialchars($page_description); ?>">
