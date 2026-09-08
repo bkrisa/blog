1647d1208b15cc65f9f4585b75f9c2ded50daa01
bkrisa <web.bkrisa@gmail.com>
2026-09-08 19:19:57 +0200

    test 4

--- app/includes/header.php
+++ app/includes/header.php
@@ -5,7 +5,7 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 // Load settings from JSON file
-require_once __DIR__ . '/../includes/settings.php';
+require_once __DIR__ . '/settings.php';
 $settings = loadSettings();
 $root_path = getRootPath();
 
@@ -27,7 +27,7 @@ function getSafeDescription(string $rawDesc): string {
 }
 
 // Load schema functions
-//require_once __DIR__ . '/schema.php';
+require_once __DIR__ . '/schema.php';
 
 // Check if the current page is a dashboard page
 $uri = $_SERVER['REQUEST_URI'];
